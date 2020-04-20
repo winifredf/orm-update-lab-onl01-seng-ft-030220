926f5678652f39d0944c55536967aa3c200920e4
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-20 07:15:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ class Student
     
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
-    
+end  
   
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
