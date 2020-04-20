dc6d268d561b3e8a925354c860cb02e542618281
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-20 07:38:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,9 +53,12 @@ end
   end
 
   def self.new_from_db(row)
-    new_song = self.new
-    new_student_id = row[0]
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
     new_student
+  end
     
 
 end
