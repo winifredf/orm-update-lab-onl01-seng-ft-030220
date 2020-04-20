7bec38e1ac950489b3b78dfb64ab1568f7599547
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-20 06:51:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,16 @@ class Student
       @grade = grade
     end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      name TEXT,
+      grade TEXT
+      )
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
