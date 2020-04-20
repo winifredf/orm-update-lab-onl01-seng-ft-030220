21b3d07c4ef54750d3d1e473c1c11359f5937e84
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-20 07:22:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
       VALUES (?, ?)
     SQL
     
-    DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
     
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
