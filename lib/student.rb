1014ad543fb6b86d1505c79ced2504253c1c793e
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-20 07:04:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
     
     DB[:conn].execute(sql, self.name, self.grade)
     
-    @id = DB[:conn].execute("SELECT last_inserted_rowid() FROM students")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
     
   # Remember, you can access your database connection anywhere in this class
