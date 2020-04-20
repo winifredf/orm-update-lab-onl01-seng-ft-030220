7133f76cd0d6a4eefd509163a97a0dd702929234
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-20 07:13:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,10 +41,7 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
     
-  def update
-    sql = "UPDATE students SET name = Jane_Smith, album = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.grade, self.id)
-  end
+  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
