1a59f5859ce4657ebe1eb1bb4ff1f2fcb8646cc6
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-20 07:55:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,7 +69,10 @@ end
     end.first
   end
   
-  
+  def update
+    sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.id)
+  end
 end
 
 
